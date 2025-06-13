8a426eea53a8e156e8443586812d346a998cef00
Arielle Sousa <ariellekellen7@gmail.com>
2025-06-13 15:49:19 -0300

    ajustes no api e rotas

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,19 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ArticleController;
 
-Route::apiResource('articles', ArticleController::class);
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+Route::apiResource('articles', ArticleController::class)->names([
+    'index' => 'api.articles.index',
+    'store' => 'api.articles.store',
+    'show' => 'api.articles.show',
+    'update' => 'api.articles.update',
+    'destroy' => 'api.articles.destroy',
+]);
+
